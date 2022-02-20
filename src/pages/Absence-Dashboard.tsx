b4df2f0df3c181287b417263564940d91bb90ca5
Vinicius Verona <verona.projects@tutanota.com>
2022-02-20 16:43:06 -0300

    Display absences and number of absences
    * Display total number of absences and absences table without paginating

--- src/pages/Absence-Dashboard.tsx
+++ src/pages/Absence-Dashboard.tsx
@@ -1,28 +1,18 @@
-import { Member, getMembers } from "../data/members";
-import { Absence, getAbsences } from "../data/absences";
+import { Member, MEMBERS } from "../data/members";
+import { Absence, ABSENCES } from "../data/absences";
+import { Dashboard } from "../styles/AbsenceDashboard";
+import AbsenceTable from "../components/Absence-Table";
 
 export default function AbsenceDashboard() {
-    let absences: Map<number, Absence> = getAbsences();
-    let members: Map<number, Member> = getMembers();
+    let absences: Map<number, Absence> = ABSENCES;
+    let members: Map<number, Member> = MEMBERS;
 
     return (
-        <div>
-            <h1>Hello World!</h1>
-            <p>Project with some configurations</p>
-
+        <Dashboard>
             <p>
-                There is a total of {members.size} members and {absences.size}{" "}
-                absences in the project.
+                Total number of absences: <strong>{absences.size}</strong>.
             </p>
-
-            {Array.from(absences).map((key) => {
-                return (
-                    <p key={key[0]}>
-                        Absence <strong>{key[0]}</strong> is related to{" "}
-                        <strong>{members.get(key[1].userId).name}</strong>.
-                    </p>
-                );
-            })}
-        </div>
+            <AbsenceTable absences={absences} members={members} />
+        </Dashboard>
     );
 }
--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -2,6 +2,8 @@
 export default {
     palettes: {
         primary: "rgba(255, 148, 25, 1)",
+        lightPrimary: "rgba(255, 148, 25, 0.5)",
         light: "#DFDFDF",
+        darkerLight: "#B5B5B5",
     },
 };
