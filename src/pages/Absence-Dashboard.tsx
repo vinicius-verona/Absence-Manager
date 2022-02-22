55b532b5de7bd65cb8c403d80470f6997b643c50
Vinicius Verona <verona.projects@tutanota.com>
2022-02-21 21:01:37 -0300

    Add loading state to dashboard

--- src/pages/Absence-Dashboard.tsx
+++ src/pages/Absence-Dashboard.tsx
@@ -1,18 +1,54 @@
 import { Member, MEMBERS } from "../data/members";
 import { Absence, ABSENCES } from "../data/absences";
 import { Dashboard } from "../styles/AbsenceDashboard";
+import { useState, useEffect } from "react";
+import { PulseLoader } from "react-spinners";
 import AbsenceTable from "../components/Absence-Table";
+import Theme from "../styles/Theme";
+
+const overrideLoading = `
+  display: block;
+  margin: 40vmin auto;
+  align-self: center;
+  justify-self: center;
+  border-color: red;
+`;
+
+type JSONData = {
+  absences: Map<number, Absence>;
+  members: Map<number, Member>;
+};
 
 export default function AbsenceDashboard() {
-    let absences: Map<number, Absence> = ABSENCES;
-    let members: Map<number, Member> = MEMBERS;
+  let [loading, setLoading] = useState(false);
+  let [absences, setAbsences] = useState(new Map<number, Absence>());
+  let [members, setMembers] = useState(new Map<number, Member>());
 
-    return (
-        <Dashboard>
-            <p>
-                Total number of absences: <strong>{absences.size}</strong>.
-            </p>
-            <AbsenceTable absences={absences} members={members} />
-        </Dashboard>
-    );
+  useEffect(() => {
+    setLoading(true);
+    setAbsences(ABSENCES);
+    setMembers(MEMBERS);
+    setTimeout(() => setLoading(false), 2000);
+    // setLoading(false);
+  }, [ABSENCES, MEMBERS]);
+
+  return (
+    <Dashboard>
+      {loading ? (
+        <PulseLoader
+          css={overrideLoading}
+          color={Theme.palettes.primary}
+          loading={loading}
+          size={20}
+        />
+      ) : (
+        <div className="absence-table">
+          <p>
+            Total number of absences: <strong>{absences.size}</strong>.
+          </p>
+          <AbsenceTable absences={absences} members={members} />
+        </div>
+      )}
+    </Dashboard>
+  );
 }
